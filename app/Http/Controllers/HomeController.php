30b5e6d5c4e42d9440d4f73167f76b4b32e70d76
Nannapas <nannapas4869@gmail.com>
2023-01-13 22:35:17 +0700

    try to get view in filament
    still not successful

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Filament\Facades\Filament;
+use Illuminate\Contracts\View\View;
+class HomeController extends Controller
+{
+
+}
