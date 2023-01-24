8414f7af2514bb004f3ea9d1b1f1e53582f186a8
Nannapas <nannapas4869@gmail.com>
2023-01-24 20:15:01 +0700

    make dashboard
    ยังไม่เสร็จ

--- resources/views/filament/widgets/dash-board-wid-get.blade.php
+++ resources/views/filament/widgets/dash-board-wid-get.blade.php
@@ -7,15 +7,30 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,400;0,500;0,600;0,700;0,800;0,900;1,400&display=swap" rel="stylesheet">
-    <style>
-        body {
-            font-family: 'Kanit' !important;
-        }
-    </style>
+    <link rel="stylesheet" href="{{asset('assets/css/global.css')}}"/>
 </head>
 <body>
-    <div class="container m-auto grid grid-cols-1 gap-1 bg-white">
-        <div class="">วัน</div>
+    <div class="days">
+        <span class="day">วัน</span>
+        <span class="day-code">รหัสความเสียหาย</span>
+        <br>
+        <span class="day-ten">10</span>
+        <span class="code">A</span>
+        <span class="items">3 รายการ</span>
+
+        <br>
+        <span class="code-2">B</span>
+        <span class="items-2">2 รายการ</span>
+        <br>
+        <span class="code-3">C</span>
+        <span class="items-3">1 รายการ</span>
+        <br>
+        <span class="code-4">D</span>
+        <span class="items-4">4 รายการ</span>
+        <span class="item">รายการ</span>
+        <div class="left"></div>
+
     </div>
+
 
 </body>
